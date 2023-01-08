ee307621da53b634d4b4eff33b70f2c8ae5e8c38
MikhailAErshov <aem.cmd@gmail.com>
2023-01-08 18:01:21 +0300

    4th commit

--- WelcomeGit.java
+++
@@ -1,6 +0,0 @@
-public class WelcomeGit {
-    public static void main(String[] args) {
-        System.out.println("Welcome from Git!");
-        System.out.println("Welcome from Git!");
-    }
-}
---
+++ src/WelcomeFromGit.java
@@ -0,0 +1,5 @@
+public class WelcomeFromGit {
+    public static void main(String[] args) {
+        System.out.println("Privet");
+    }
+}
